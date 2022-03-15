46b6444e9e9c4d8efa2ec16e650ab548d1752786
Sarkasaa <zockipod@gmail.com>
2022-03-15 21:14:18 +0100

    first

---
+++ code.py
@@ -0,0 +1,41 @@
+import time
+import usb_hid
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard import Keyboard
+import board
+import digitalio
+import supervisor
+
+
+class button:
+    def __init__(self, pin, keycode):
+        self.keycode = keycode
+        self.lastpressed = False
+        self.btn = digitalio.DigitalInOut(pin)
+        self.btn.direction = digitalio.Direction.INPUT
+        self.btn.pull = digitalio.Pull.DOWN
+
+    def update(self):
+        if not self.lastpressed and self.btn.value:
+            keyboard.send(self.keycode)
+        self.lastpressed = self.btn.value
+
+
+keyboard = Keyboard(usb_hid.devices)
+
+supervisor.disable_autoreload()
+
+
+buttons = [
+    button(board.GP10, Keycode.KEYPAD_ONE),
+    button(board.GP11, Keycode.KEYPAD_TWO),
+    button(board.GP12, Keycode.KEYPAD_THREE),
+    button(board.GP19, Keycode.KEYPAD_FOUR),
+    button(board.GP20, Keycode.KEYPAD_FIVE),
+    button(board.GP21, Keycode.KEYPAD_SIX),
+]
+
+while True:
+    for b in buttons:
+        b.update()
+    time.sleep(0.001)
